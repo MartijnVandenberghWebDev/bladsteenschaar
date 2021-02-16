3193d6a52ed0d962d4e18e1ee2b1bd8907298941
Martijn Vandenbergh <vandenbergh.martijn@gmail.com>
2021-02-16 14:27:26 +0100

    fix

--- bss.js
+++ bss.js
@@ -4,21 +4,30 @@ const steen = document.getElementById("steen");
 const schaar = document.getElementById("schaar");
 const bladSteenSchaar = [blad, steen, schaar];
 
-
-
 for (const afbeelding of document.querySelectorAll("#initialiseer a")) {
     afbeelding.onclick = function () {
         //Tonen keuze gebruiker:
+        const keuzeGebruiker = `${this.dataset.nummer}`;
         const keuzeImg = document.getElementById("keuzeImg");
         keuzeImg.src = `${this.dataset.naam}.png`;
-        
+
         //Keuze van de computer:
         const computerImg = document.getElementById("computerImg");
         const keuzeComputer = Math.floor(Math.random() * 3);
-        console.log("keuze computer:",keuzeComputer);
         computerImg.src = `${(bladSteenSchaar[keuzeComputer]).dataset.naam}.png`
 
         //Berekening winnaar:
-        
-    };
+        const winnaar = document.getElementById("winnaar");
+        if (keuzeGebruiker === `${keuzeComputer}`) {
+            winnaar.innerText = "Gelijkspel.";
+        } else if (keuzeGebruiker === 0 && `${keuzeComputer}` === 1) {
+            winnaar.innerText = "Winnaar 1";
+        } else if (keuzeGebruiker === 1 && `${keuzeComputer}` === 2) {
+            winnaar.innerText = "Winnaar 2";
+        } else if (keuzeGebruiker === 2 && `${keuzeComputer}` === 0) {
+            winnaar.innerText = "Winnaar 3";
+        } else {
+            winnaar.innerText = "Default."
+        }
+    }
 }
